3504eadace9b3d50305d56b015a5e049f7a552be
eddah73 <eddahsjr@gmail.com>
2024-09-07 00:16:33 +0300

    SQL

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears(name,age,sex,color,temperament,alive)
+VALUES 
+('Mr. Chocolate', 5, 'M', 'Brown', 'Sweet', 1),
+('Rowdy', 7, 'M', 'Gray', 'Mischievous', 1),
+('Tabitha', 3, 'F', 'White', 'Gentle', 1),
+('Sergeant Brown', 10, 'M', 'Black', 'Disciplined', 1),
+('Melissa', 6, 'F', 'Golden', 'Playful', 1),
+('Grinch', 8, 'M', 'Green', 'Grumpy', 1),
+('Wendy', 4, 'F', 'Tan', 'Curious', 1),
+(NULL, NULL, NULL, NULL,'unarmed',NULL);
